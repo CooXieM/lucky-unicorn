aec9200e351d853ed32220c6d11a5a4b1efe8c9d
CooXieM <99296416+CooXieM@users.noreply.github.com>
2022-02-16 11:42:12 +1300

    Add files via upload

--- fun.py
+++ fun.py
@@ -1,4 +1,7 @@
 import math
+
+PI = math.pi
+
 radius = float(input('enter radius of the circle : '))
-circumference = 2 * math.pi * radius
+circumference = 2 * PI * radius
 print('circumference of circle is : %.2f' % circumference)
